244c4e3e9817a067f317f67a99158134960150b4
Yih-Chun Hu <git@matrii.com>
2022-09-06 11:55:55 -0500

    lecture

--- calculator.asm
+++ calculator.asm
@@ -15,12 +15,18 @@ LOOPTOP			; loop that takes input
 	JSR PUSH    	 ; perform the push
 	BRnzp LOOPTOP	 ; done with this character, get next one
 NOT_A_NUMBER  		 ; not a digit, but maybe an op
-	ADD R1, R1, #5	 ; if R0 == '+', then R1 == -5
+	ADD R1, R1, #3	 ; if R0 == '-', then R1 == -3
+	BRz DO_SUBTRACT	 ; == '-'
+	ADD R1, R1, #2	 ; if R0 == '+', then R1 was -5, now -2
 	BRz DO_ADD  	 ; == '+'
 	ADD R1, R1, #1	 ; if R0 == '*', then R1 was -6, now -1
 	BRz DO_MULTIPLY	 ; == '*'
 	BRnzp LOOPTOP	 ; not a recognized operator, get next character
 
+DO_SUBTRACT
+	JSR STACK_SUB
+	BRnzp LOOPTOP
+	
 DO_ADD
 	JSR STACK_ADD
 	BRnzp LOOPTOP
@@ -29,6 +35,18 @@ DO_MULTIPLY
 	JSR STACK_MULT
 	BRnzp LOOPTOP
 
+STACK_SUB
+	ST R7, SOMEWHERE_ELSE
+	JSR POP
+	NOT R1, R0
+	ADD R1, R1, #1			; R1: negative operand #2
+	JSR POP				; R0: operand 1
+	ADD R0, R0, R1
+	JSR PUSH
+	LD R7, SOMEWHERE_ELSE		; Restore R7 so RET is not infinite loop
+	RET    				; go back to the function that called us
+	
+	
 STACK_ADD
 	ST R7, SOMEWHERE_ELSE		; our JSRs will clobber R7, better save
 	       				; it (somewhere else)
@@ -47,12 +65,14 @@ STACK_MULT
 	ADD R1, R0, #0   ; R1 <- R0	; move the first operand to R1
 	JSR POP	    	      	 	; call pop to get the second operand
 	    				; R0 <- R0 * R1
-	AND R3, R3, #0
+	AND R3, R3, #0			; clear R3
 MULTLOOP
-	ADD R3, R0, R3
-	ADD R1, R1, #-1
-	BRnp MULTLOOP
-	ADD R0, R3, #0
+	ADD R3, R0, R3			; add R0 to R3 each time through loop
+	ADD R1, R1, #-1			; decrement R1 in the loop
+	BRnp MULTLOOP			; loop will execute (old R1) times,
+					; except: R1 is treated as unsigned int
+					; and if R1=0, run the loop 2^16 times
+	ADD R0, R3, #0			; move R3 to R0 (push expects it there)
 	JSR PUSH    			; push the result onto the stack
 	LD R7, SOMEWHERE_ELSE		; Restore R7 so RET is not infinite loop
 	RET    				; go back to the function that called us	
---
+++ train.asm
@@ -0,0 +1,74 @@
+.ORIG x3000
+
+LEA R6, STACK_END
+LEA R1, INORD	; the current inbound train car (address of/ptr)
+LEA R2, OUTORD  ; the next expected outbound train car (address of/ptr)
+
+LOOPTOP
+	LDR R3, R1, #0	; R3 is the current inbound train car name
+	NOT R3, R3
+	ADD R3, R3, #1
+	LDR R4, R2, #0	; R4 is the next expected outbound train car name
+	BRz ALLDONE
+	ADD R3, R3, R4
+	BRz DO_THROUGH
+	; if the stack is empty, push
+	LEA R3, STACK_END
+	NOT R3, R3
+	ADD R3, R3, #1
+	ADD R3, R3, R6
+	BRz DO_PUSH
+	; R0 <- top of the stack, could also directly LDR, could also make PEEK
+	JSR POP
+	JSR PUSH
+	NOT R0, R0
+	ADD R0, R0, #1
+	ADD R0, R0, R4
+	BRz DO_POP
+
+DO_PUSH
+	LD R0, PUSHOUT
+	OUT
+	LDR R0, R1, #0	; R0 is the current inbound train car name
+	JSR PUSH
+	ADD R1, R1, #1
+	BRnzp LOOPTOP
+
+DO_POP
+	LD R0, POPOUT
+	OUT
+	JSR POP
+	ADD R2, R2, #1
+	BRnzp LOOPTOP
+
+DO_THROUGH
+	LD R0, THROUGHOUT
+	OUT
+	ADD R1, R1, #1
+	ADD R2, R2, #1
+	BRnzp LOOPTOP
+
+ALLDONE
+	HALT
+
+INORD .STRINGZ "ABCD"  ; pointer to current (input) car in R1
+OUTORD .STRINGZ "DCBA" ; pointer to current (expected) car in R2
+THROUGHOUT .STRINGZ "T"
+PUSHOUT .STRINGZ "U"
+POPOUT .STRINGZ "O"
+
+PUSH
+	ADD R6, R6, #-1
+	STR R0, R6, #0
+	RET
+
+POP
+	LDR R0, R6, #0
+	ADD R6, R6, #1
+	RET
+
+SOMEWHERE_ELSE .FILL x0
+STACK_LIVES_HERE .BLKW #30
+STACK_END .FILL xBEEF
+
+.END
